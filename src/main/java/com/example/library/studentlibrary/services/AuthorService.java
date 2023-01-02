3fd5943c80f83cbf5f63d846f8bcb31163b6f1cb
piyush21malhotra <101010021+piyush21malhotra@users.noreply.github.com>
2023-01-02 18:41:43 +0530

    success

--- src/main/java/com/example/library/studentlibrary/services/AuthorService.java
+++ src/main/java/com/example/library/studentlibrary/services/AuthorService.java
@@ -11,7 +11,7 @@ public class AuthorService {
     @Autowired
     AuthorRepository authorRepository1;
 
-    public void create(Author author){
-        authorRepository1.save(author);
+    public void create(Author authors){
+        authorRepository1.save(authors);
     }
 }
